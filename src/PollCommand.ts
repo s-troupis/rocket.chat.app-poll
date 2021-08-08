8bf6daad26745fd61cc4a90b22db0dbbb858f5ed
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-08-08 12:46:06 +0530

    [IMPROVE] Removes template subcommand and modal

--- src/PollCommand.ts
+++ src/PollCommand.ts
@@ -1,7 +1,6 @@
 import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
 import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
 import { createPollModal } from './lib/createPollModal';
-import { createTemplateModal } from './lib/createTemplateModal';
 
 export class PollCommand implements ISlashCommand {
     public command = 'poll';
@@ -12,32 +11,17 @@ export class PollCommand implements ISlashCommand {
     public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<void> {
         const triggerId = context.getTriggerId();
 
-        const [subcommand] = context.getArguments();
-
         const data = {
             room: (context.getRoom() as any).value,
             threadId: context.getThreadId(),
         };
 
-        if (subcommand === 'template') {
+        const question = context.getArguments().join(' ');
 
-            const question = context.getArguments().slice(1).join(' ');
+        if (triggerId) {
+            const modal = await createPollModal({ question, persistence: persis, modify, data });
 
-            if (triggerId) {
-                const modal = await createTemplateModal({ question, persistence: persis, modify, data });
-
-                await modify.getUiController().openModalView(modal, { triggerId }, context.getSender());
-            }
-
-        } else {
-
-            const question = context.getArguments().join(' ');
-
-            if (triggerId) {
-                const modal = await createPollModal({ question, persistence: persis, modify, data });
-
-                await modify.getUiController().openModalView(modal, { triggerId }, context.getSender());
-            }
+            await modify.getUiController().openModalView(modal, { triggerId }, context.getSender());
         }
     }
 }
